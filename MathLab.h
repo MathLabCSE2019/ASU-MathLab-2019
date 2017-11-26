e9694b3381c555154bda0ec2b2d3f691db2499d1
Team1Leader <32913400+Team1Leader@users.noreply.github.com>
2017-11-26 22:27:02 +0200

    final .h file

--- MathLab.h
+++ MathLab.h
@@ -1,34 +1,42 @@
+#pragma once
+
 #include <iostream>
 #include <string>
+
+using namespace std;
+enum MI{ MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE };
+
 class CMatrix{
+    //int nR, nC;
+    //double** values;
+public:
     int nR, nC;
     double** values;
-    public:
-     CMatrix();
-    ~CMatrix();
-    enum MI{MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE};
-    CMatrix(int nR, int nC, int initialization = MI_ZEROS, double initializationValue = 0.0);
-    CMatrix(int nR, int nC, double **values);
-    CMatrix(int nR, int nC, double first, ...);
-    CMatrix(CMatrix& m);
-    CMatrix(double d);
-    CMatrix(string s);
-    void copy(CMatrix& m);
-    void copy(double d);
-    void copy(string s);
-    void reset();
+    void print();
+    CMatrix();//
+    ~CMatrix();//
+    //enum MI{ MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE };//
+    CMatrix(int nR, int nC, int initialization = MI_ZEROS, double initializationValue = 0.0);//
+    CMatrix(int nR, int nC, double first, ...);//
+    CMatrix(CMatrix& m);//
+    CMatrix(double d);//
+    CMatrix(string s);//
+    void copy(const CMatrix& m);//
+    void copy(double d);//
+    void copy(string s);//
+    void reset();//
     string getString();
-    CMatrix operator=(CMatrix& m);
-    CMatrix operator=(double d);
-    CMatrix operator=(string s);
-    void add(CMatrix& m);
-    void operator+=(CMatrix& m);
-    void operator+=(double d);
+    CMatrix operator=(const CMatrix& m);//
+    CMatrix operator=(double d); //
+    CMatrix operator=(string s);//
+    void add(CMatrix& m);//
+    void operator+=(CMatrix& m);//
+    void operator+=(double d);//
     CMatrix operator+(CMatrix& m);
     CMatrix operator+(double d);
-    void sub(CMatrix& m);
-    void operator-=(CMatrix& m);
-    void operator-=(double d);
+    void sub(CMatrix& m);//
+    void operator-=(CMatrix& m);//
+    void operator-=(double d);//
     CMatrix operator-(CMatrix& m);
     CMatrix operator-(double d);
     void mul(CMatrix& m);
@@ -36,37 +44,43 @@ class CMatrix{
     void operator*=(double d);
     CMatrix operator*(CMatrix& m);
     CMatrix operator*(double d);
-    void div(CMatrix& m);
+   // CMatrix operator.*(CMatrix& m);//-----------------
+
     void operator/=(CMatrix& m);
-    void operator/=(double d);
+    void operator/=(double d);//
     CMatrix operator/(CMatrix& m);
     CMatrix operator/(double d);
-    CMatrix operator++();
-     //Pre Increment
-	CMatrix operator++(int);
-     //Post Increment, int is not used
-	CMatrix operator--();
-     //Pre Increment
-	CMatrix operator--(int);
-     //Post Increment, int is not used
-	CMatrix operator-();
+    CMatrix element_div(CMatrix& another_matrix);
+    //CMatrix operator./(CMatrix& another_matrix); //-----------------
+    CMatrix operator++();//
+    //Pre Increment
+    CMatrix operator++(int);
+    //Post Increment, int is not used
+    CMatrix operator--();//
+    //Pre Increment
+    CMatrix operator--(int);
+    //Post Increment, int is not used
+    CMatrix operator-();
     CMatrix operator+();
     friend istream& operator >> (istream &is, CMatrix& C);
-     //Stream
-	friend ostream& operator << (ostream &os, CMatrix& C);
-     //Stream
-	void setSubMatrix(int iR, int iC, CMatrix& m);
+    //Stream
+    friend ostream& operator << (ostream &os, CMatrix& C);
+    //Stream
+    void setSubMatrix(int iR, int iC, CMatrix& m);
     CMatrix getSubMatrix(int r, int c, int nr, int nc);
     CMatrix getCofactor(int r, int c);
     void addColumn(CMatrix& m);
     void addRow(CMatrix& m);
-    double& operator[](int i){return values[i/nC][i%nC];}
-    double& operator()(int i){return values[i/nC][i%nC];}
-    double& operator()(int r, int c){return values[r][c];}
-    int getn(){return nR*nC;};
-    int getnR(){return nR;};
-    int getnC(){return nC;};
+    double& operator[](int i){ return values[i / nC][i%nC]; }
+    double& operator()(int i){ return values[i / nC][i%nC]; }
+    double& operator()(int r, int c){ return values[r][c]; }
+    int getn(){ return nR*nC; };//
+    int getnR(){ return nR; };//
+    int getnC(){ return nC; };//
     double getDeterminant();
-    double getTranspose();
-    double getInverse();
+    void getTranspose(CMatrix another_matrix);
+    void getInverse(CMatrix another_matrix);
+    void div(CMatrix& m);
+    CMatrix element_div(double a);
+    CMatrix element_mul(CMatrix& another_matrix);
 };
