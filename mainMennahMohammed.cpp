87e1fc8ae931fdbf9781904bc53cf579c91cf624
Menna Allah Muhammed Abd el rahman <menna.muhammed198@gmail.com>
2018-01-27 19:58:12 +0200

    varChar fn to define the input variable name.

--- mainMennahMohammed.cpp
+++ mainMennahMohammed.cpp
@@ -1,2 +1,12 @@
 
 string cleanString (string x) { for(int i =0;i<x.size();i++){ if(x.substr(i,1)==x.substr(i+1,1)&&x.substr(i,1)==" ") {x. erase(i,1) ; i--;} } return x; }
+string varChar (string x){ //find var before '=' sign
+        istringstream readInput(x);
+        string varTemp = "";
+        getline(readInput, varTemp,'=');
+        for(int i=0; i<varTemp.length(); i++){
+            if(varTemp[i] == ' ') varTemp.erase(i,1);
+        }
+        if(!char_exist(x,"="))return x.substr(0,1);
+        return varTemp;
+}
