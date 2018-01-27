78198ffdce7a32ee887b498c8186b32fbe628a35
Shimaa Hassan <eng.shimaa71296@gmail.com>
2018-01-27 21:37:26 +0200

    fixed error in writing and condition

--- mainShimaa.cpp
+++ mainShimaa.cpp
@@ -14,44 +14,47 @@ if(in.is_open()){
     input[0] = cleanString(input[0]);
     cout<<input[0]<<endl;
     string tvar = varChar(input[0]);
-       if(!exist(vars,tvar)){ //var not exist
+    if(!exist(vars,tvar)){ //var not exist
         if(char_exist(input[0],"=")){ //for multi-line
                 vars.push_back(tvar);
                 cout<<"**"<<tvar<<endl;}
     int inputBegNum = countChar(input[0],'[');
     int inputEndNum = countChar(input[0],']');
-     if((inputBegNum>0||inputEndNum>0)&&(inputBegNum == inputEndNum)&&(nLine == 1)){ //check for one line input
+    if((inputBegNum>0||inputEndNum>0)&&(inputBegNum == inputEndNum)&&(nLine == 1)){ //check for one line input
       nLine = 1;
       cout<<"one line input!"<<endl;
-       }}
-     else if(inputBegNum>0||inputEndNum>0){ //check for multi-line input
+    }else if(inputBegNum>0||inputEndNum>0){ //check for multi-line input
       cout<<"multi-line input!"<<endl;
        if(nLine==1){input[1] = input[0];
        nLine = 2;}
        else{
        cout<<"this is the full line"<<endl<<input[1]<<endl<<input[0]<<endl;
         nLine = 1;nV--;}}
-       else if(char_exist(input[0],"=")){ // this is operation
+   else if(char_exist(input[0],"=")){ // this is operation
    //cout<<"operation!"<<endl;
-    vector <string> varsInOperation;
+   vector <string> varsInOperation;
    int n = 0;
-charsExistInString(input[0],varsInOperation,n); // know all matrix variables in the operation  
-   if(n == 0 && operationExist(input[0])){ // return 1*1 matrix
-     varValues[nV] = oneToOneOperationCalculation(input[0]);
+charsExistInString(input[0],varsInOperation,n); // know all matrix variables in the operation
+if(n == 0 && operationExist(input[0])){ // return 1*1 matrix
+varValues[nV] = oneToOneOperationCalculation(input[0]);
 //cout << varValues[nV]<<endl;
-   }
-   else if (n==0){ // return MI Matrix
-
-} else { // return R*C Matrix
+}
+else if (n==0){ // return MI Matrix
 
 }
-         else if(input[0]!=""){ // print var line
+/*for(int i=0; i<n;i++){
+    cout<<varsInOperation[i]<<"&&&";
+}cout<<n<<endl;*/
+else { // return R*C Matrix
+
+}
+   }
+   else if(input[0]!=""){ // print var line
    cout<<"Variable Does Not Exist!"<<endl;
    nV--;
    }
    else{nV--;} //for empty
 nV++;
-}}
 }else{ // var exist
    if(input[0]!="" && input[0] != "\n"){ // print var line
     int pos = pos_search(vars,tvar);
@@ -59,7 +62,6 @@ nV++;
    }
 }
 }}
-}
 else{cout<<"File Does Not Open"<<endl;}
 return 0;
 }
