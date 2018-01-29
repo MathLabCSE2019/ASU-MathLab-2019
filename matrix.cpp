0a0233ceb71d5a323e86c367a0e9856f41bbef0e
Mahmoud Hamdy <mahmoudhamdy.eeng@gmail.com>
2018-01-29 23:33:05 +0200

    Integration with Group 2
    New Features :
    - Trignometric functions
    - squareroot function (per element only)

--- matrix.cpp
+++ matrix.cpp
@@ -878,6 +878,485 @@ CMatrix CMatrix::mlog() //ln function
 }
 
 
+
+    /**________________________________________________**/
+    /**_________Matrix Trignometric Operation__________**/
+    /**________________________________________________**/
+
+
+//-----------------------------------SinFunction---------------------------------------
+CMatrix Sin(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = sin(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------CosFunction---------------------------------------
+CMatrix Cos(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = cos(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------TanFunction---------------------------------------
+CMatrix Tan(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = tan(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//---------------------------------CscFunction----------------------------------------
+CMatrix Csc(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+           result_matrix.values[i][j] = 1/sin(matrix.values[i][j]);
+        }
+
+    }
+    return result_matrix;
+}
+
+//---------------------------------SecFunction----------------------------------------
+CMatrix Sec(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+           result_matrix.values[i][j] = 1/cos(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//---------------------------------CotFunction----------------------------------------
+CMatrix Cot(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+           result_matrix.values[i][j] = 1/tan(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------------------SindFunction------------------------------------
+CMatrix Sind(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    //float pi = 3.141592654;
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+           result_matrix.values[i][j] = sin((pi/180)*matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------------------CosdFunction---------------------------------
+CMatrix Cosd(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    //float pi = 3.141592654;
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = cos((pi/180)*matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------------------TandFunction----------------------------------------------------
+CMatrix Tand(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    //float pi = 3.141592654;
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = tan((pi/180)*matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------------------CscdFunction----------------------------------------------------
+CMatrix Cscd(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    //float pi = 3.141592654;
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = 1/sin((pi/180)*matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------------------SecdFunction---------------------------------------------------
+CMatrix Secd(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    //float pi = 3.141592654;
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = 1/cos((pi/180)*matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//-----------------------------------------------CotdFunction----------------------------------------------------
+CMatrix Cotd(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    //float pi = 3.141592654;
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = 1/tan((pi/180)*matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//--------------------------------------------asinFunction------------------------------------------------------
+CMatrix aSin(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = asin(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//--------------------------------------------acosFunction------------------------------------------------------
+CMatrix aCos(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = acos(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//--------------------------------------------atanFunction------------------------------------------------------
+CMatrix aTan(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = atan(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//--------------------------------------------asecFunction------------------------------------------------------
+CMatrix aSec(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = acos(1/matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//--------------------------------------------acscFunction------------------------------------------------------
+CMatrix aCsc(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = asin(1/matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+//--------------------------------------------acotFunction------------------------------------------------------
+CMatrix aCot(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR,matrix.nC);
+    for(int i=0 ; i<matrix.nR ; i++)
+    {
+        for(int j=0 ; j<matrix.nC ; j++)
+        {
+            result_matrix.values[i][j] = atan(1/matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+
+
+
+//-----------------------------------------SquareRootElementByElement---------------------------
+/** friend function **/
+ CMatrix squareRoot(CMatrix matrix)
+{
+    CMatrix result_matrix(matrix.nR , matrix.nC);
+    for(int i=0;i<matrix.nR;i++)
+    {
+        for(int j=0;j<matrix.nC;j++)
+        {
+            result_matrix.values[i][j] = sqrt(matrix.values[i][j]);
+        }
+    }
+    return result_matrix;
+}
+//=======================================================================================================
+//=======================================SquareRootForAllMatrix================================================
+/*
+void CMatrix:: sqrtm(CMatrix& matrix)
+{
+     struct matrix
+    {
+    double real;
+    double imag;
+    };
+    //----------------------Calculate the race and Determinant----------------------------
+    double Trace = values[0][0]+values[1][1];
+    double Determinant = values[0][0]*values[1][1]-values[0][1]*values[1][0];
+    //----------------------Calculate EigenValues-----------------------------------------
+    matrix eigenValue1;
+    matrix eigenValue2;
+    double square = (pow(Trace,2)/4)-Determinant;
+    if(square > 0)
+    {
+    eigenValue1.real = (Trace/2)+sqrt(square);
+    eigenValue2.real = (Trace/2)-sqrt(square);
+    eigenValue1.imag = 0;
+    eigenValue2.imag = 0;
+    }
+    else if(square < 0)
+    {
+    square *= -1;
+    eigenValue1.imag = (Trace/2)+sqrt(square);
+    eigenValue2.imag = (Trace/2)-sqrt(square);
+    eigenValue1.real = 0;
+    eigenValue2.real = 0;
+    }
+    else
+    {
+    eigenValue1.real = (Trace/2);
+    eigenValue2.real = (Trace/2);
+    }
+    //----------------------Calculate firstEigenVector------------------------------------
+    matrix **eigenVector1 = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+        eigenVector1[i] = new matrix[1];
+        for(int j=0;j<1;j++)
+        {
+            eigenVector1[i][j].real = 0;
+            eigenVector1[i][j].imag = 0;
+        }
+    }
+    eigenVector1[0][0].real = eigenValue1.real-values[1][1];
+    eigenVector1[0][0].imag = eigenValue1.imag;
+    eigenVector1[1][0].real = values[1][0];
+    //----------------------secondEigenVector---------------------------------------------
+     matrix **eigenVector2 = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+        eigenVector2[i] = new matrix[1];
+        for(int j=0;j<1;j++)
+        {
+            eigenVector2[i][j].real = 0;
+            eigenVector2[i][j].imag = 0;
+        }
+    }
+    eigenVector2[0][0].real = eigenValue2.real-values[1][1];
+    eigenVector2[0][0].imag = eigenValue2.imag;
+    eigenVector2[1][0].real = values[1][0];
+    //----------------------------------eigenVectorMatrix----------------------------------
+      matrix **eigenVectorMatrix = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+        eigenVectorMatrix[i] = new matrix[nC];
+         for(int j=0;j<nC;j++)
+        {
+            eigenVectorMatrix[i][j].real = 0;
+            eigenVectorMatrix[i][j].imag = 0;
+        }
+    }
+    eigenVectorMatrix[0][0].real = eigenVector1[0][0].real;
+    eigenVectorMatrix[0][0].imag = eigenVector1[0][0].imag;
+    eigenVectorMatrix[1][0].real = eigenVector1[1][0].real;
+    eigenVectorMatrix[1][0].imag = eigenVector1[1][0].imag;
+    eigenVectorMatrix[0][1].real = eigenVector2[0][0].real;
+    eigenVectorMatrix[0][1].imag = eigenVector2[0][0].imag;
+    eigenVectorMatrix[1][1].real = eigenVector2[1][0].real;
+    eigenVectorMatrix[1][1].imag = eigenVector2[1][0].imag;
+    //----------------------------------eigenValuesMatrix---------------------------------------------
+    matrix **eigenValuesMatrix = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+         eigenValuesMatrix[i] = new matrix[nC];
+         for(int j=0;j<nC;j++)
+         {
+            eigenValuesMatrix[i][j].real = 0;
+            eigenValuesMatrix[i][j].imag = 0;
+         }
+    }
+    eigenValuesMatrix[0][0].real = real(sqrt(complex<double>(eigenValue1.real, eigenValue1.imag)));
+    eigenValuesMatrix[0][0].imag = imag(sqrt(complex<double>(eigenValue1.real, eigenValue1.imag)));
+    eigenValuesMatrix[1][1].real = real(sqrt(complex<double>(eigenValue2.real, eigenValue2.imag)));
+    eigenValuesMatrix[1][1].imag = imag(sqrt(complex<double>(eigenValue2.real, eigenValue2.imag)));
+    //----------------------------------eigenVectorsInverseMatrix---------------------------------------------
+      matrix **eigenVectorsInverseMatrix = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+        eigenVectorsInverseMatrix[i] = new matrix[nC];
+        for(int j=0;j<nC;j++)
+        {
+            eigenVectorsInverseMatrix[i][j].real = 0;
+            eigenVectorsInverseMatrix[i][j].imag = 0;
+        }
+    }
+    matrix DeterminantForInverse,DInverse;
+    DeterminantForInverse.real = ((eigenVectorMatrix[0][0].real*eigenVectorMatrix[1][1].real)-(eigenVectorMatrix[0][0].imag*eigenVectorMatrix[1][1].imag))- ((eigenVectorMatrix[0][1].real*eigenVectorMatrix[1][0].real)-(eigenVectorMatrix[0][1].imag*eigenVectorMatrix[1][0].imag));
+    DeterminantForInverse.imag = ((eigenVectorMatrix[0][0].real*eigenVectorMatrix[1][1].imag)+(eigenVectorMatrix[0][0].imag*eigenVectorMatrix[1][1].real))-((eigenVectorMatrix[0][1].real*eigenVectorMatrix[1][0].imag)+(eigenVectorMatrix[0][1].imag*eigenVectorMatrix[1][0].real));
+    complex <double> z (DeterminantForInverse.real,DeterminantForInverse.imag);
+    DInverse.real = real(pow(z,-1));
+    DInverse.imag = imag(pow(z,-1));
+    eigenVectorsInverseMatrix[0][0].real = ((DInverse.real*eigenVectorMatrix[1][1].real)-(DInverse.imag*eigenVectorMatrix[1][1].imag));
+    eigenVectorsInverseMatrix[0][0].imag = ((DInverse.real*eigenVectorMatrix[1][1].imag)+(DInverse.imag*eigenVectorMatrix[1][1].real));
+    eigenVectorsInverseMatrix[1][0].real = ((DInverse.real*eigenVectorMatrix[1][0].real)-(DInverse.imag*eigenVectorMatrix[1][0].imag))*(-1);
+    eigenVectorsInverseMatrix[1][0].imag = ((DInverse.real*eigenVectorMatrix[1][0].imag)+(DInverse.imag*eigenVectorMatrix[1][0].real))*(-1);
+    eigenVectorsInverseMatrix[0][1].real = ((DInverse.real*eigenVectorMatrix[0][1].real)-(DInverse.imag*eigenVectorMatrix[0][1].imag))*(-1);
+    eigenVectorsInverseMatrix[0][1].imag = ((DInverse.real*eigenVectorMatrix[0][1].imag)+(DInverse.imag*eigenVectorMatrix[0][1].real))*(-1);
+    eigenVectorsInverseMatrix[1][1].real = ((DInverse.real*eigenVectorMatrix[0][0].real)-(DInverse.imag*eigenVectorMatrix[0][0].imag));
+    eigenVectorsInverseMatrix[1][1].imag = ((DInverse.real*eigenVectorMatrix[0][0].imag)+(DInverse.imag*eigenVectorMatrix[0][0].real));
+    //----------------------------------Result-----------------------------------------
+     matrix **Result1 = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+        Result1[i] = new matrix[nC];
+         for (int j = 0; j < nC; j++)
+        {
+            Result1[i][j].real = 0;
+            Result1[i][j].imag = 0;
+        }
+    }
+     for (int i = 0; i < nR; i++)
+    {
+        for (int j = 0; j < nC; j++)
+        {
+
+            for (int k = 0; k < nR; k++)
+            {
+                Result1[i][j].real += (eigenVectorMatrix[i][k].real * eigenValuesMatrix[k][j].real) - (eigenVectorMatrix[i][k].imag * eigenValuesMatrix[k][j].imag) ;
+                Result1[i][j].imag += (eigenVectorMatrix[i][k].real * eigenValuesMatrix[k][j].imag) + (eigenVectorMatrix[i][k].imag * eigenValuesMatrix[k][j].real) ;
+            }
+        }
+
+    }
+     matrix **Result = new matrix*[nR];
+    for(int i=0;i<nR;i++)
+    {
+        Result[i] = new matrix[nC];
+           for (int j = 0; j < nC; j++)
+        {
+            Result[i][j].real = 0;
+            Result[i][j].imag = 0;
+        }
+    }
+     for (int i = 0; i < nR; i++)
+    {
+        for (int j = 0; j < nC; j++)
+        {
+
+            for (int k = 0; k < nR; k++)
+            {
+                 Result[i][j].real += (Result1[i][k].real * eigenVectorsInverseMatrix[k][j].real) - (Result1[i][k].imag * eigenVectorsInverseMatrix[k][j].imag) ;
+                 Result[i][j].imag += (Result1[i][k].real * eigenVectorsInverseMatrix[k][j].imag) + (Result1[i][k].imag * eigenVectorsInverseMatrix[k][j].real) ;
+            }
+        }
+
+    }
+    //----------------------------------Printing the Result---------------------------------------------
+    for(int i=0 ; i<nR ; i++)
+    {
+        for(int j=0 ; j<nC ; j++)
+        {
+
+            if(Result[i][j].imag>=0)
+            {
+            cout<<Result[i][j].real<<'+'<<Result[i][j].imag<<'i'<<"\t";
+            }
+            else if(Result[i][j].imag<0)
+            {
+            cout<<Result[i][j].real<<Result[i][j].imag<<'i'<<"\t";
+            }
+            else
+            {
+            cout<<Result[i][j].real<<'+'<<Result[i][j].imag<<'i'<<"\t";
+            }
+        }
+        cout<<"\n";
+    }
+
+}
+*/
+
+
+
 /**=================================================================**/
 
     /**_____________________________________________**/
