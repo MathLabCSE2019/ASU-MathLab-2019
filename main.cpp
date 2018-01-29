f288da3c874fc21d9b8b3777bc2a39b467b7f737
Mahmoud Hamdy <mahmoudhamdy.eeng@gmail.com>
2018-01-29 05:48:20 +0200

    Fixes and Enhancements after Phase 1
    - div in big example is fixed
    - operators work effiecientle
    - lvalue erros is fixed
    - code is more organized
    - new functions from phase 2 are added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "MathLab.h"
+#include "matrix.h"
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -7,67 +7,66 @@
 #include <vector>
 using namespace std;
 
-CMatrix dummy_matrix;
 //----------------------------------------
 int pos_search(vector <string> x, string c){ //will need it to know where a var exists already
-int pos = 0;
-for(int i=0;i<x.size();i++){
-    for(int j=0;j<x[i].length();j++){
-        if(x[i].substr(j,1)==" ")x[i].erase(i,1);
+    int pos = 0;
+    for(int i=0;i<x.size();i++){
+        for(int j=0;j<x[i].length();j++){
+            if(x[i].substr(j,1)==" ")x[i].erase(i,1);
+        }
+        for(int j=0;j<c.length();j++){
+            if(c.substr(j,1)==" ")c.erase(i,1);
+        }
+        if( !x[i].compare(c)){
+            pos = i;
+        }
     }
-    for(int j=0;j<c.length();j++){
-        if(c.substr(j,1)==" ")c.erase(i,1);
-    }
-    if( !x[i].compare(c)){
-        pos = i;
-    }
-}
-return pos;
+    return pos;
 }
 
 
 bool char_exist(string x, string y){ //Will need to know if certain char exist in string
-bool exist = false;
-int siz = x.size();
-int i =0;
-for(i=0;i<siz;i++){
-    if(x.substr(i,1)=="."){
-        if(x.substr(i,2)==y){
-            exist =true;
-            i++;
+    bool exist = false;
+    int siz = x.size();
+    int i = 0;
+    for(i=0;i<siz;i++){
+        if(x.substr(i,1)=="."){
+            if(x.substr(i,2)==y){
+                exist =true;
+                i++;
+            }
         }
-     }
-    else if(x.substr(i,1)==y){
-        exist = true;
+        else if(x.substr(i,1)==y){
+            exist = true;
+        }
     }
-}
-return exist;
+    return exist;
 }
 
 string operation(string x){
-string y = "";
-if(char_exist(x,"+")){
-    y +="+";
-}
-else if(char_exist(x,"-")){
-    y +="-";
-}
-else if(char_exist(x,"./")){
-    y +="./";
-}
-else if(char_exist(x,".*")){
-    y +=".*";
-}
-else if(char_exist(x,"*")){
-    y +="*";
-}
-else if(char_exist(x,"/")){
-    y +="/";
-}
-else if(char_exist(x,"\'")){
-    y +="\'";
-}
-return y;
+    string y = "";
+    if(char_exist(x,"+")){
+        y +="+";
+    }
+    else if(char_exist(x,"-")){
+        y +="-";
+    }
+    else if(char_exist(x,"./")){
+        y +="./";
+    }
+    else if(char_exist(x,".*")){
+        y +=".*";
+    }
+    else if(char_exist(x,"*")){
+        y +="*";
+    }
+    else if(char_exist(x,"/")){
+        y +="/";
+    }
+    else if(char_exist(x,"\'")){
+        y +="\'";
+    }
+    return y;
 }
 
 bool exist(vector <string> x, string c){ //will need it to know if a var exists or not
@@ -91,11 +90,11 @@ string varChar (string x){ //find var before '=' sign
 }
 
 string aVarChar (string x, bool bfOp){ //to find vars to do operations on bfOp true to find before operation false to find after
-        x=x.substr(x.find_first_of('=')+1);
-        istringstream readInput(x);
-        string varTemp = "";
-        string op = operation(x);
-        if(bfOp){
+    x=x.substr(x.find_first_of('=')+1);
+    istringstream readInput(x);
+    string varTemp = "";
+    string op = operation(x);
+    if(bfOp){
         getline(readInput, varTemp,op[0]);
         for(int i=0; i<varTemp.length(); i++){
             if(varTemp[i] == ' ') varTemp.erase(i,1);
@@ -103,147 +102,146 @@ string aVarChar (string x, bool bfOp){ //to find vars to do operations on bfOp t
             if(varTemp[i] == '/') varTemp.erase(i,1);
             if(varTemp[i] == '*') varTemp.erase(i,1);
         }
-        }
-        else{
-            varTemp = x.substr(x.find_first_of(op)+1,x.length()-x.find_first_of(op)+1);
-            for(int i=0; i<varTemp.length(); i++){
-                if(varTemp[i] == ' ') varTemp.erase(i,1);
-                if(varTemp[i] == ';') varTemp.erase(i,1);
-                if(varTemp[i] == '.') varTemp.erase(i,1);
+    }
+    else{
+        varTemp = x.substr(x.find_first_of(op)+1,x.length()-x.find_first_of(op)+1);
+        for(int i=0; i<varTemp.length(); i++){
+            if(varTemp[i] == ' ') varTemp.erase(i,1);
+            if(varTemp[i] == ';') varTemp.erase(i,1);
+            if(varTemp[i] == '.') varTemp.erase(i,1);
 
-            }
         }
+    }
 
-        return varTemp;
+    return varTemp;
 }
 
 
 int char_pos(string x,char c){
-int pos = NULL;
-for(int i=0;i<x.length();i++){
-    if(x[i]==c){
-        pos =i;
+    int pos = 0;  /** pos is edited from NULLL to 0 **/
+    for(int i=0;i<x.length();i++){
+        if(x[i]==c){
+            pos =i;
         }
 
     }
-return pos;
+    return pos;
 }
 
 void opCalc(CMatrix a, CMatrix b, CMatrix& c, string o){
-       if(o == "+"){
+    if(o == "+"){
         c = a + b;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "-"){
-        a.sub(b);
-        c=a;
+    }
+    else if(o == "-"){
+        c = a - b;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "*"){
-        a.mul(b);
-        c=a;
+    }
+    else if(o == "*"){
+        c = a * b;
         cout<<endl<<c<<endl;
-       }
-        else if(o == "./"){
-       c =  a.element_div(b);
+    }
+    else if(o == "./"){
+        c =  a.element_div(b);
         cout<<endl<<c<<endl;
-       }
-       else if(o == "/"){
-       dummy_matrix = b;
-        c=a;
-        c.div(dummy_matrix);
+    }
+    else if(o == "/"){
+        c = a / b;
         cout<<endl<<c<<endl;
-       }
-       else if(o==".*"){
+    }
+    else if(o==".*"){
         c =  a.element_mul(b);
         cout<<endl<<c<<endl;
-       }
-       else if(o == "\'"){
-        dummy_matrix.getTranspose(a);
-        c=dummy_matrix;
+    }
+    else if(o == "\'"){
+        c = a.getTranspose();
         cout<<endl<<c<<endl;
-       }
+    }
 }
+
+
 void opCalc(double a, CMatrix b, CMatrix& c,string o){
-       if(o == "+"){
-        b+=a;
+    if(o == "+"){
         c = b;
+        c+=a;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "-"){
-        b*=-1;
-        b+=a;
+    }
+    else if(o == "-"){
+        c = b * -1;
+        c+=a;
+        cout<<endl<<c<<endl;
+    }
+    else if(o == "*"){
+        c = b * a;
+        cout<<endl<<c<<endl;
+    }
+    else if(o == "/"){
+        //c = b.element_div(a);
+        c = b.getInverse();
+        c *= a;
+        cout<<endl<<c<<endl;
+    }
+    else if(o == "./"){
+        //c = b.element_div(a);
+        c = b.getInverse();
+        c *= a;
+        cout<<endl<<c<<endl;
+    }
+    else if(o== ".*"){
         c = b;
+        c *= a;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "*"){
-        b*=a;
-        c =b;
-        cout<<endl<<c<<endl;
-       }
-       else if(o == "/"){
-        c = b.element_div(a);
-        cout<<endl<<c<<endl;
-       }
-       else if(o == "./"){
-        c = b.element_div(a);
-        cout<<endl<<c<<endl;
-        cout<< "NO";
-       }
-       else if(o== ".*"){
-        b*=a;
-        c = b;
-        cout<<endl<<c<<endl;
-       }
-       }
+    }
+}
 
 void opCalc(CMatrix a, double b, CMatrix& c,string o){
-       if(o == "+"){
+    if(o == "+"){
         c= a + b;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "-"){
+    }
+    else if(o == "-"){
         c= a - b;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "*"){
-        a*=b;
-        c=a;
+    }
+    else if(o == "*"){
+        c = a*b;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "/"){
-       c= a / b;
+    }
+    else if(o == "/"){
+        c = a / b;
         cout<<endl<<c<<endl;
-       }
-       else if(o == "./"){
-        c= a / b;
+    }
+    else if(o == "./"){
+        c = a / b;
         cout<<endl<<c<<endl;
-       }
-       else if(o== ".*"){
-        a*=b;
+    }
+    else if(o== ".*"){
         c = a;
+        c *= b;
         cout<<endl<<c<<endl;
-       }
+    }
 }
 
 bool numExist(string x){
     bool res=false;
-    if(char_exist(x,"1")||char_exist(x,"2")||char_exist(x,"3")||char_exist(x,"4")||char_exist(x,"5")||char_exist(x,"6")||char_exist(x,"7")||char_exist(x,"8")||char_exist(x,"9")||char_exist(x,"0")){
+    if(char_exist(x,"1")||char_exist(x,"2")||char_exist(x,"3")||
+       char_exist(x,"4")||char_exist(x,"5")||char_exist(x,"6")||
+       char_exist(x,"7")||char_exist(x,"8")||char_exist(x,"9")||char_exist(x,"0")){
+
         res =true;
     }
     return res;
 }
 
-
 int main (int argc, char* argv[]){
     //---------------------input---------------------------------------------------------------------
-vector <string> vars;
-CMatrix varValues[1000];
-int i = 0 ;
-string input = "";
-//----file-------------------------
-ifstream testFile("example(1).m");
-      if(testFile.is_open()){
+    vector <string> vars;
+    CMatrix varValues[1000];
+    int i = 0 ;
+    string input = "";
+    //----file-------------------------
+    ifstream testFile(argv[1]);
+        if(testFile.is_open()){
 
             while( !testFile.eof() ){
                 getline(testFile,input);
@@ -351,6 +349,7 @@ ifstream testFile("example(1).m");
         bool eq_exist = char_exist(input,"=");
         if(eq_exist){
            bool op_exist = char_exist(input,"+") ||char_exist(input,"-")||char_exist(input,"*")||char_exist(input,"/")||char_exist(input,"./")||char_exist(input,".*")||char_exist(input,"\'");
+           //string tvar = varChar(input);
            string tvar = varChar(input);
            if(!exist(vars,tvar)){ //var not exist
                 vars.push_back(tvar);
